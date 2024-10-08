6ff316f6e456e886663b884e1151b8d3597396b3
Felix Toledo <felixtoledoctes@gmail.com>
2024-10-08 18:43:01 -0300

    correcion de error de app, sacar cosas de cart

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const addToCartController = require('./addToCart/addToCartController');  
 const sculptureRoutes= require('./routes/sculptureRoutes');
 const sculptorRoutes= require('./routes/sculptorRoutes');
 
 const app = express();
 app.use(express.json());
-app.use('/carrito', addToCartController);  // Integra el router
 app.use('/sculptures', sculptureRoutes);
 app.use('/sculptors', sculptorRoutes);
 
